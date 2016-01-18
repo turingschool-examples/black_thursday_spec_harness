b15205e1c6243401d2950ef60881fe83e75004dc
Nick Weiland <nickweiland@gmail.com>
2016-01-17 22:13:06 -0700

    Remove duplicat word, Iter 1, line 61

--- spec/iteration_1_spec.rb
+++ spec/iteration_1_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe "Iteration 1" do
     end
 
     it "#average_average_price_per_merchant returns the average price for all merchants" do
-      expected = sales_analyst.average_average_price_per_merchant
+      expected = sales_analyst.average_price_per_merchant
 
       expect(expected).to eq 349.56
       expect(expected.class).to eq BigDecimal
